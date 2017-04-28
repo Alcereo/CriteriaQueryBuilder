27b182dddf7a27f5539cc42b4c00f185803d825c
a.kucheev <ak-91@mail.ru>
2017-04-28 11:11:38 +0300

    update

--- src/main/java/ru/alcereo/usability/Predictive.java
+++ src/main/java/ru/alcereo/usability/Predictive.java
@@ -1,13 +1,16 @@
 package ru.alcereo.usability;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.From;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Map;
 
 /**
  * Created by alcereo on 27.04.17.
  */
 public interface Predictive {
 
-    Predicate buildCriteriaPredicate(final CriteriaBuilder cb);
+    Predicate buildCriteriaPredicate(final CriteriaBuildData data);
 
 }
